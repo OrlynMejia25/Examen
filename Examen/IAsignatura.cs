25dbb88bf5de557f4d0707856e37fdd7a0528965
OrlynMejia25 <orlyncastro@gmail.com>
2024-08-13 12:46:04 -0600

    interface asignatura

---
+++ Examen/IAsignatura.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Examen
+{
+    public interface IAsignatura
+    {
+        double CalcularNotaFinal();
+        double CalcularNotaFinal(int no1, int no2, int no3);
+        string MensajeNotaFinal(double notaFinal);
+        void Imprimir();
+
+    }
+}
